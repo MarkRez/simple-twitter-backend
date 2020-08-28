395f9ef94868ab2201df795ad761f9efe8a51351
Mark <mark.slavenko.rezet@gmail.com>
2020-08-28 15:54:34 +0300

    Create route groups

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,36 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    echo "<h1>Hello! I am a Laravel server!</h1>";
+});
+
+Route::prefix('api')->group(function () {
+    Route::prefix('posts')->group(function () {
+        Route::get('/', 'PostController@getAll');
+        Route::get('/{id}', 'PostController@getById');
+        Route::post('/{id}/addComment', 'PostController@addComment');
+        Route::post('/', 'PostController@add');
+        Route::put('/{id}', 'PostController@Update');
+        Route::delete('/{id}', 'PostController@delete');
+    });
+
+    Route::prefix('users')->group(function () {
+        Route::get('/', 'UserController@getAll');
+        Route::get('/{id}', 'UserController@getById');
+        Route::post('/', 'UserController@add');
+        Route::put('/{id}', 'UserController@update');
+        Route::delete('/{id}', 'UserController@delete');
+    });
+
+    Route::prefix('feed')->group(function () {
+        Route::get('/', 'FeedController@getAll');
+        Route::get('/from-followed', 'FeedController@getFromFollowers');
+    });
+
+    Route::prefix('dialogs')->group(function () {
+        Route::get('/', 'DialogsController@getAll');
+        Route::get('/{id}', 'DialogsController@getById');
+        Route::post('/{id}/send', 'DialogsController@sendMessage');
+    });
 });

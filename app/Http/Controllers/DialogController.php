7b535e0c0ccd04199878be41cc1e771443d511be
Mark <mark.slavenko.rezet@gmail.com>
2020-10-02 16:37:50 +0300

    Create DialogController + Dialog model

---
+++ app/Http/Controllers/DialogController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class DialogController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        // TODO get dialogs list
+    }
+}

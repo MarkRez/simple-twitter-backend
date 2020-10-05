c21ab110daa38a40f04bb3895459fcc3cc3a77b9
Mark <mark.slavenko.rezet@gmail.com>
2020-10-05 10:06:07 +0300

    Refactor DialogsList to new structure (with dialogs table)

--- resources/js/routes/Messages/DialogsList/DialogsList.jsx
+++ resources/js/routes/Messages/DialogsList/DialogsList.jsx
@@ -7,27 +7,32 @@ import './dialogsList.scss';
 
 const DialogsList = ({ dialogs }) => (
   <div className="dialogs-list w-100 p-0">
-    {(dialogs || []).map(dialog =>
-      <Link key={dialog.id} to={`${ROUTES.MESSAGES}/${dialog.id}`}>
-        <div className='dialog w-100 py-3 px-4'>
-          <div className="row">
-            <div className="col-lg-1">
-              {dialog.avatar
-                ? <img src={dialog.avatar} onError={handleImageError} alt="name" className="img-fluid rounded-circle"/>
-                : <Skeleton height={50} width={50} circle={true}/>
-              }
-            </div>
-            <div className="col-lg-11">
-              <p className="mb-2">
-                {dialog.name || <Skeleton width={100} height={20}/>}
-              </p>
-              <p className="overflow-hidden">
-                {dialog.last_message || <Skeleton/>}
-              </p>
+    {(dialogs || []).map(dialog => {
+      const {user = {}} = dialog;
+      return (
+        <Link key={dialog.id} to={`${ROUTES.MESSAGES}/${dialog.id}`}>
+          <div className='dialog w-100 py-3 px-4'>
+            <div className="row">
+              <div className="col-lg-1">
+                {user.avatar
+                  ? <img src={user.avatar} onError={handleImageError} alt="name" className="img-fluid rounded-circle"/>
+                  : <Skeleton height={50} width={50} circle={true}/>
+                }
+              </div>
+              <div className="col-lg-11">
+                <p className="mb-2">
+                  {dialog.name || user.name || <Skeleton width={100} height={20}/>}
+                </p>
+                <p className="overflow-hidden">
+                  {dialog.last_message || <Skeleton/>}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </Link>
+        </Link>
+      )
+    }
+
     )}
   </div>
 )

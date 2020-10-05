ec397ddd39815ea80f77bb84b4d919a09308f1cc
Mark <mark.slavenko.rezet@gmail.com>
2020-10-05 16:46:22 +0300

    Refactor index method from MessageController

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -7,19 +7,15 @@ use App\Http\Requests\SendMessageRequest;
 use App\Http\Resources\ContactedUserResource;
 use App\Http\Resources\MessageResource;
 use App\Http\Resources\MessagesCollection;
+use App\Models\Dialog;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    public function dialogs(Request $request)
+    public function index(Dialog $dialog)
     {
-        return ContactedUserResource::collection($request->user()->getContactedUsers());
-    }
-
-    public function index(Request $request, User $user)
-    {
-        return new MessagesCollection($request->user()->getMessagesWithUser($user->id)->paginate(15), $user);
+        return new MessagesCollection($dialog->messages()->paginate(15), $dialog->dialogUsers()->first());
     }
 
     public function create(User $user, SendMessageRequest $request)

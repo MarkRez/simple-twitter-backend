a8f98855858ef09965595cdb5e28ba1192f24a62
Mark <mark.slavenko.rezet@gmail.com>
2020-10-05 10:09:06 +0300

    Add new methods to Dialog model

--- app/Models/Dialog.php
+++ app/Models/Dialog.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Dialog extends Model
 {
@@ -14,5 +15,15 @@ class Dialog extends Model
     public function users()
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public function dialogUsers()
+    {
+        return $this->users()->where('id', '!=', Auth::id())->get();
+    }
+
+    public function lastMessage()
+    {
+        return $this->messages()->take(1)->latest()->first();
     }
 }

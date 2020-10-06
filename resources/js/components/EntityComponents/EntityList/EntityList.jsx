dc31175d23bcb66632ad438f054cd2fe507a6183
Mark <mark.slavenko.rezet@gmail.com>
2020-10-06 13:21:21 +0300

    Delete List variable from EntityList component

--- resources/js/components/EntityComponents/EntityList/EntityList.jsx
+++ resources/js/components/EntityComponents/EntityList/EntityList.jsx
@@ -6,17 +6,14 @@ export const EntityList = (
   {
     entities,
     ...props
-  }) => {
-  const List = (entities || []).map(post => (<ListEntity
-    key={post.id}
-    data={post}
-    {...props}
-  />));
-
-
-  return (
-    <div className="user-posts w-100 p-0">
-      {List}
-    </div>
-  )
-}
+  }) => (
+  <div className="user-posts w-100 p-0">
+    {
+      (entities || []).map(post => (<ListEntity
+        key={post.id}
+        data={post}
+        {...props}
+      />))
+    }
+  </div>
+);

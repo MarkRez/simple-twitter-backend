8d0d3861a2ba22c3fccf737a0e91b2a25ecb7495
Mark <mark.slavenko.rezet@gmail.com>
2020-10-07 09:02:56 +0300

    Add new route constants

--- resources/js/helpers/routes.js
+++ resources/js/helpers/routes.js
@@ -1,9 +1,13 @@
 export const ROUTES = {
   LOGIN: '/login',
+  LOGOUT: '/logout',
+  PROFILE: '/profile',
   REGISTRATION: '/registration',
   USERS: '/users',
   POSTS: '/posts',
   FEED: '/feed',
+  TAGS: '/tags',
+  DIALOGS: '/dialogs',
   MESSAGES: '/messages',
   RECOVERY: '/recovery',
   EDIT: '/edit-profile'
